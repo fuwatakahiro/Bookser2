21bfdf0d735ff83cf6a72f1c233f02fe6b6b411f
Fuwatakahiro <110452794+fuwatakahiro@users.noreply.github.com>
2022-08-22 08:46:19 +0000

    必要なMVC OK

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  # def index
+  # @books = Book.all
+  # @book = Book.new
+  # end
+  # def create
+  # @book = Book.new(book_params)
+  # @book.user_id = current_user.id
+  # @book.save
+  # redirect_to "#"
+  # end
+  # def show
+  # @book = Book.find(params[:id])
+  # end
+  # def edit
+  # @book = Book.find(params[:id])
+  # end
+  # def update
+  #   @book = Book.find(params[:id])
+  #   @book.save(book_params)
+  #   redirect_to "#"
+  # end
+  # def destroy
+  #   @book = Book.find(params[:id])
+  #   @book.destroy
+  #   redirect_to "#"
+  # end
+  
+  # private
+  # def book_params
+  #   params.require(:book).permit(:title, :body)
+  # end
+end
